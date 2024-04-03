234ca9a1a692724d50cf33219df234fdec5a5c26
Sharanam Chotai <sharanamchotai@gmail.com>
2024-04-03 16:33:42 +0530

    Feat: Add channel filtering functionality

--- frontend.jsx
+++ frontend.jsx
@@ -24,6 +24,7 @@ function App() {
   const [message, setMessage] = createSignal("");
 
   const [incomingMessages, setIncomingMessages] = createSignal({});
+  const [hiddenChannels, setHiddenChannels] = createSignal([]);
 
   ws.onmessage = async (event) => {
     let [channel, message] = event.data.split(/:(.+)/).map((x) => x.trim());
@@ -71,58 +72,95 @@ function App() {
   return (
     <>
       <h1>Redis Pub/Sub</h1>
-      <input
-        id="message"
-        type="text"
-        autofocus
-        placeholder="Send to Redis Pub/Sub"
-        value={message()}
-        onInput={(e) => setMessage(e.target.value)}
-        onKeyUp={handleSubmit}
-      />
-      <input
-        type="text"
-        id="channel"
-        placeholder="Channel name"
-        value={channel()}
-        onInput={(e) => setChannel(e.target.value)}
-        onKeyUp={handleSubmit}
-      />
+      <div className="row-1">
+        <div className="publisher">
+          <h2>Publisher</h2>
+          <input
+            id="message"
+            type="text"
+            autofocus
+            placeholder="Send to Redis Pub/Sub"
+            value={message()}
+            onInput={(e) => setMessage(e.target.value)}
+            onKeyUp={handleSubmit}
+          />
+          <input
+            type="text"
+            id="channel"
+            placeholder="Channel name"
+            value={channel()}
+            onInput={(e) => setChannel(e.target.value)}
+            onKeyUp={handleSubmit}
+          />
+        </div>
+      </div>
+      {/* <div className="filter-container">
+        <textarea
+          id="filter"
+          placeholder="Filter messages"
+          aria-label="Filter messages"
+          onInput={(e) => {
+            setHiddenChannels(
+              Object.keys(incomingMessages()).filter(
+                (channel) =>
+                  !incomingMessages()[channel].some((message) =>
+                    message.includes(e.target.value)
+                  )
+              )
+            );
+          }}
+        ></textarea>
+        <button onClick={() => setHiddenChannels([])}>Show all</button>
+        <button
+          onClick={() => setHiddenChannels(Object.keys(incomingMessages()))}
+        >
+          Hide all
+        </button>
+      </div> */}
+      <p>Channels</p>
       <div className="channels">
         <For each={Object.keys(incomingMessages())}>
           {(channel) => (
             <button
-              onClick={() =>
-                setIncomingMessages((prev) => {
-                  delete prev[channel];
-                  return { ...prev };
-                })
-              }
+              className="channel-btn"
+              onClick={() => {
+                if (hiddenChannels().includes(channel)) {
+                  setHiddenChannels((prev) =>
+                    prev.filter((ch) => ch !== channel)
+                  );
+                } else {
+                  setHiddenChannels((prev) => [...prev, channel]);
+                }
+              }}
             >
-              ❌ {channel}
+              {hiddenChannels().includes(channel) ? "🟪 " : "☑️ "} {channel}
             </button>
           )}
         </For>
       </div>
       <div id="messages">
         <For
-          each={Object.keys(incomingMessages())}
+          each={Object.keys(incomingMessages()).filter(
+            (channel) => !hiddenChannels().includes(channel)
+          )}
           fallback={<p>No messages</p>}
         >
           {(channel) => (
-            <p>
-              <strong>{channel}</strong>
-              <Show when={incomingMessages()[channel]?.length}>
-                <For each={incomingMessages()[channel]}>
-                  {(message) => (
-                    <span
-                      className="unit"
-                      innerHTML={message}
-                    ></span>
-                  )}
-                </For>
-              </Show>
-            </p>
+            <Show when={!hiddenChannels().includes(channel)}>
+              <p>
+                <strong>{channel}</strong>
+                <Show when={incomingMessages()[channel]?.length}>
+                  <For each={incomingMessages()[channel]}>
+                    {(message) => (
+                      <span
+                        className="unit"
+                        innerHTML={message}
+                      ></span>
+                    )}
+                  </For>
+                </Show>
+              </p>
+            </Show>
           )}
         </For>
       </div>
